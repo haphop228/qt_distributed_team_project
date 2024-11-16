383b32276b3b2a218071e7a46b39200bc074b72f
alicee <avega2003@yandex.ru>
2024-11-16 10:38:22 +0300

    now docker images are build for linux/arm64 and linux/amd64

--- docker_servers/push_all_images_to_docker_hub.sh
+++ docker_servers/push_all_images_to_docker_hub.sh
@@ -2,32 +2,50 @@
 
 # Переменные
 DOCKER_USERNAME="alice3e"
-#DOCKER_PASSWORD="your_dockerhub_password"  # Замените на свой пароль или используйте Docker Secrets для безопасности
 IMAGE_TAG="latest"
 
 # Массив с именами сервисов
 SERVERS=("main_server" "mongo_app" "sqlite_app" "worker_node_control_server")
 
 # Логин в Docker Hub
-echo "Logging in to Docker Hub..."
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#echo "Logging in to Docker Hub..."
+#docker login -u "$DOCKER_USERNAME" || { echo "Failed to log in to Docker Hub"; exit 1; }
+
+# Проверяем, включён ли buildx
+if ! docker buildx version >/dev/null 2>&1; then
+    echo "Docker Buildx не установлен. Пожалуйста, установите Buildx и попробуйте снова."
+    exit 1
+fi
+
+# Проверяем, есть ли активный builder
+if docker buildx inspect multiarch_builder > /dev/null 2>&1; then
+    echo "Using existing multiarch_builder..."
+    docker buildx use multiarch_builder
+else
+    echo "Creating and using new multiarch_builder..."
+    docker buildx create --name multiarch_builder --use --bootstrap
+fi
+docker buildx inspect --bootstrap
+
+# Убедимся, что buildx используется
+docker buildx create --use --name multiarch_builder --bootstrap || echo "Buildx builder already exists and is in use."
 
 # Функция для сборки и пуша образов
 build_and_push_image() {
     local server_name=$1
-    echo "Building Docker image for $server_name..."
-    docker build -t "$DOCKER_USERNAME/$server_name:$IMAGE_TAG" ./$server_name
+    echo "Building multi-architecture Docker image for $server_name..."
+
+    # Сборка и публикация образа для нескольких архитектур
+    docker buildx build \
+        --platform linux/amd64,linux/arm64 \
+        -t "$DOCKER_USERNAME/$server_name:$IMAGE_TAG" \
+        "./$server_name" \
+        --push
+
     if [ $? -eq 0 ]; then
-        echo "Successfully built $server_name image."
-        echo "Pushing $server_name image to Docker Hub..."
-        docker push "$DOCKER_USERNAME/$server_name:$IMAGE_TAG"
-        if [ $? -eq 0 ]; then
-            echo "$server_name image successfully pushed to Docker Hub."
-        else
-            echo "Failed to push $server_name image."
-        fi
+        echo "Successfully built and pushed $server_name image for multi-architecture."
     else
-        echo "Failed to build $server_name image."
+        echo "Failed to build or push $server_name image."
     fi
 }
 
@@ -36,4 +54,4 @@ for server in "${SERVERS[@]}"; do
     build_and_push_image "$server"
 done
 
-echo "All images have been processed."
+echo "All images have been successfully built and pushed for multi-architecture."
